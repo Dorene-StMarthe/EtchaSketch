6a31db1170b329d760ae9e84021aeea5c3e33587
Dorene07 <dstmarth@gmail.com>
2024-08-18 14:19:00 -0400

    created grid border

---
+++ script.js
@@ -0,0 +1,9 @@
+const grid = document.querySelector(".grid");
+
+for (let i=0; i<256; i++){
+    
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        grid.appendChild(div);
+      
+    }
